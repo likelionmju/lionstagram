58f5d0a8aef7d218c3e312a843d4682dce49426f
Jeonghun Ban <bjh5230@gmail.com>
2019-04-01 13:44:17 +0900

    home.html에 queryset 출력

--- page/views.py
+++ page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects
+    return render(request, 'home.html', {'posts':posts})
