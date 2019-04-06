1fd5556c51da62917afb97020c35c7e54eb2b581
Jeonghun Ban <bjh5230@gmail.com>
2019-04-07 00:40:25 +0900

    수정 및 삭제 기능 추가

--- page/urls.py
+++ page/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:post_id>/', views.post_detail, name='post_detail'),
     path('new/', views.post_new, name='post_new'),
+    path('delete/<int:post_id>', views.post_delete, name='post_delete'),
+    path('edit/<int:post_id>', views.post_edit, name='post_edit'),
 ]
--- page/views.py
+++ page/views.py
@@ -8,8 +8,8 @@ def home(request):
     return render(request, 'home.html', {'posts':posts})
 
 def post_detail(request, post_id):
-    detail = get_object_or_404(Post, pk=post_id)
-    return render(request, 'post_detail.html', {'detail':detail})
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post_detail.html', {'post':post})
 
 def post_new(request):
     if request.method == 'POST':
@@ -25,4 +25,30 @@ def post_new(request):
         return redirect('/post/'+str(post.id))
     else:
         return render(request, 'post_new.html')
+
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author == request.user:
+        post.delete()
+        return redirect('home')
+    else:
+        return redirect('post_detail', post_id)
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    # 수정 제출
+    if request.method == 'POST':
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        # image 파일이 있으면 post 객체에 저장
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.save()
+        return redirect('/post/'+str(post.id))
+    else:
+        if post.author == request.user:
+            return render(request, 'post_edit.html', {'post':post})
+        else:
+            return redirect('home')
+        
     
