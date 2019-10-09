8d5fc61686a63c11ee113e8909d1dbc3d97b48a8
Jeonghun-Ban <bjh5230@gmail.com>
2019-10-10 01:15:06 +0900

    Create Profile Model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# 유저 정보 import
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='profile/', blank=True)
+
+    def __str__(self):
+        return self.name
