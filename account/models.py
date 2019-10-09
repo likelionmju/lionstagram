8e411a733c9c4f162144ecb1be2145eca12e815e
Jeonghun-Ban <bjh5230@gmail.com>
2019-10-10 01:42:37 +0900

    Modify Profile Model

--- account/models.py
+++ account/models.py
@@ -3,8 +3,5 @@ from django.db import models
 from django.conf import settings
 
 class Profile(models.Model):
-    name = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile/', blank=True)
-
-    def __str__(self):
-        return self.name
