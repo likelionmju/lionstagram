3b4cd738fdc7e300d97b457fabea39c6730af170
Jeonghun-Ban <bjh5230@gmail.com>
2019-10-10 02:40:36 +0900

    Add Function: change-profile-image

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('userpage/<str:author_id>', views.userpage, name='userpage'),
+    path('changeprofile/', views.change_profile, name='change_profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import auth
 from page.models import Post
+from account.models import Profile
 
 # Create your views here.
 def register(request):
@@ -39,3 +40,13 @@ def userpage(request, author_id):
     # 해당 유저의 포스트만 불러오기
     posts = Post.objects.filter(author=user)
     return render(request, 'userpage.html', {'posts':posts, 'user':user})
+
+
+def change_profile(request):
+    profile = Profile()
+    profile.user = request.user
+    # image 파일이 있으면 post 객체에 저장
+    if 'image' in request.FILES:
+        profile.image = request.FILES['image']
+    profile.save()
+    return redirect('/account/userpage/'+str(profile.user))
