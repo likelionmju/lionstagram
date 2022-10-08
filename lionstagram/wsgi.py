36596e9ce9865cedf408f0b31b945dd9724123e8
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-10-08 12:27:58 +0900

    chore: Rename project directory to lionstagram

--- lionstagram/wsgi.py
+++ lionstagram/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for thirdproject project.
+WSGI config for lionstagram project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thirdproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lionstagram.settings')
 
 application = get_wsgi_application()
